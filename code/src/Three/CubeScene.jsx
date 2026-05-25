0ee0027e320200265156552eddbb75091a2d4f23
luimas834 <luimas834@gmail.com>
2026-05-25 20:23:52 +0600

    added frontend template

---
+++ code/src/Three/CubeScene.jsx
@@ -0,0 +1,17 @@
+import { Canvas } from "@react-three/fiber";
+import { Stats, OrbitControls, Environment } from "@react-three/drei";
+import { Suspense } from "react";
+import Cube from "../Components/Cube";
+
+export default function CubeScene() {
+  return (
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <Suspense>
+        <Environment preset="forest" />
+      </Suspense>
+      <Cube />
+      <OrbitControls target={[0, 0, 0]} />
+      <Stats />
+    </Canvas>
+  );
+}
